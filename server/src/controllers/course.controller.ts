dc5b55858eb5bde1e0f714e77b1a24f9e764ff1b
Relix18 <relix049@gmail.com>
2024-08-01 13:08:49 +0530

    get courses for all user

--- server/src/controllers/course.controller.ts
+++ server/src/controllers/course.controller.ts
@@ -3,6 +3,7 @@ import { TryCatch } from "../middlewares/error.js";
 import { Course } from "../models/course.model.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { v2 as cloudinary } from "cloudinary";
+import { redis } from "../data/redis.js";
 
 //upload course
 export const uploadCourse = TryCatch(
@@ -57,6 +58,58 @@ export const editCourse = TryCatch(
     res.status(201).json({
       success: true,
       course,
+    });
+  }
+);
+
+//get single course -- for all users
+export const getSingleCourse = TryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const courseId = req.params.id;
+    const isCourseExist = await redis.get(courseId);
+    if (isCourseExist) {
+      const course = JSON.parse(isCourseExist);
+      return res.status(200).json({
+        success: true,
+        course,
+      });
+    }
+
+    const course = await Course.findById(req.params.id).select(
+      "-courseData.videoUrl -courseData.suggestions -courseData.questions -courseData.links"
+    );
+
+    if (!course) {
+      return next(new ErrorHandler(404, "Course not found"));
+    }
+
+    await redis.set(courseId, JSON.stringify(course));
+    res.status(200).json({
+      success: true,
+      course,
+    });
+  }
+);
+
+//get all courses -- for all users
+export const getAllCourses = TryCatch(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const isCourseExist = await redis.get("allCourses");
+    if (isCourseExist) {
+      const courses = JSON.parse(isCourseExist);
+      return res.status(200).json({
+        success: true,
+        courses,
+      });
+    }
+    const courses = await Course.find().select(
+      "-courseData.videoUrl -courseData.suggestions -courseData.questions -courseData.links"
+    );
+
+    await redis.set("allCourses", JSON.stringify(courses));
+    res.status(200).json({
+      success: true,
+      courses,
     });
   }
 );
--- server/src/routes/course.route.ts
+++ server/src/routes/course.route.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
-import { editCourse, uploadCourse } from "../controllers/course.controller.js";
+import {
+  editCourse,
+  getAllCourses,
+  getSingleCourse,
+  uploadCourse,
+} from "../controllers/course.controller.js";
 
 const router = express.Router();
 
 router.post("/create-course", isAuthenticated, isAuthorized, uploadCourse);
 router.put("/edit-course/:id", isAuthenticated, isAuthorized, editCourse);
+router.get("/get-course/:id", getSingleCourse);
+router.get("/get-courses", getAllCourses);
 
 export default router;
